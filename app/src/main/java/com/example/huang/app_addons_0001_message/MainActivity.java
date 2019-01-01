30599af289aeb4604377edf3a1546145591cbc67
EdisonHHH <499061232@qq.com>
2019-01-01 12:23:27 +0800

    new

---
+++ app/src/main/java/com/example/huang/app_addons_0001_message/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.huang.app_addons_0001_message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
+public class MainActivity extends AppCompatActivity {
+
+        private Button mButton;
+        private final String TAG="MessageText";
+        private int ButtonCount=0;
+
+        private Thread myThread;
+        private MyThread myThread2;
+        class MyRunnable implements Runnable{
+            public void run() {
+                int count=0;
+                for (;;){
+                    Log.d(TAG,"MyThread "+count);
+                    count++;
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        class MyThread extends Thread{
+            public void run(){
+                super.run();
+                int count=0;
+                for (;;){
+                    Log.d(TAG,"MyThread2 "+count);
+                    count++;
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton=(Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d(TAG,"send Message"+ButtonCount);
+                ButtonCount++;
+            }
+        });
+
+        myThread=new Thread(new MyRunnable(),"MessageTestThread");
+        myThread.start();
+        myThread2=new MyThread();
+        myThread2.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
